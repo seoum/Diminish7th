f8ab2d0872c3aa4fee80949030d2e9ff586a7094
seoum <sugowslt@gmail.com>
2024-12-02 20:40:31 +0900

    v 1.0.1 - App.js middlewares import

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import ErrorHandlerMiddleware from './middlewares/error-handler.middleware.js';
+import LogMiddleware from './middlewares/log.middleware.js';
+
 
 const app = express();
 const PORT = 3000;
+app.use(LogMiddleware);
+app.use(ErrorHandlerMiddleware);
 
 app.use(express.json());
 
